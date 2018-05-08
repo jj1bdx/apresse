fdb4b756e63d557177e7a89f4e795e92a5039343
Kenji Rikitake <kenji.rikitake@acm.org>
2018-05-08 21:44:29 +0900

    Add src/aprs_receiver.erl

---
+++ src/aprs_receiver.erl
@@ -0,0 +1,20 @@
+-module(aprs_receiver).
+
+-export([connect_dump/0]).
+
+connect_dump() ->
+    {ok, Socket} = gen_tcp:connect("japan.aprs2.net", 10152, 
+        [{active, false}, {packet, line}]),
+    {ok, Prompt} = gen_tcp:recv(Socket, 0, 5000),
+    io:format("~s~n", [Prompt]),
+    ok = gen_tcp:send(Socket, "user NOCALL pass -1 vers apresse 0.01\n"),
+    receive_loop(Socket).
+
+receive_loop(S) ->
+    case gen_tcp:recv(S, 0, 1000) of
+        {ok, D} ->
+            io:format("~s~n", [D]);
+        {error, _} ->
+            true
+    end,
+    receive_loop(S).
