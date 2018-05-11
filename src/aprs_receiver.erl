0f45c96fed8e7232232fea68482a2d3857479f2b
Kenji Rikitake <kenji.rikitake@acm.org>
2018-05-11 23:01:24 +0900

    Modify aprs_receiver to dump only 100000 messages

--- src/aprs_receiver.erl
+++ src/aprs_receiver.erl
@@ -6,15 +6,19 @@ connect_dump() ->
     {ok, Socket} = gen_tcp:connect("japan.aprs2.net", 10152, 
         [{active, false}, {packet, line}]),
     {ok, Prompt} = gen_tcp:recv(Socket, 0, 5000),
-    io:format("~s", [Prompt]),
+    % io:format("~s", [Prompt]),
+    file:write(standard_io, Prompt),
     ok = gen_tcp:send(Socket, "user NOCALL pass -1 vers apresse 0.01\n"),
-    receive_loop(Socket).
+    connect_dump_receive_loop(Socket, 100000),
+    ok = gen_tcp:close(Socket).
 
-receive_loop(S) ->
+connect_dump_receive_loop(_, 0) -> ok;
+connect_dump_receive_loop(S, N) ->
     case gen_tcp:recv(S, 0, 1000) of
         {ok, D} ->
-            io:format("~s", [D]);
+            % io:format("~s", [D]);
+            file:write(standard_io, D);
         {error, _} ->
             true
     end,
-    receive_loop(S).
+    connect_dump_receive_loop(S, N-1).
