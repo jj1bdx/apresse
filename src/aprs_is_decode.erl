aed815356b39ad7a5a72f114c9ac7347c8fa77b2
Kenji Rikitake <kenji.rikitake@acm.org>
2018-05-22 14:37:07 +0900

    Add more types

--- src/aprs_is_decode.erl
+++ src/aprs_is_decode.erl
@@ -24,6 +24,12 @@ info_dispatch(Info) ->
 
 info_dispatch_type($!, Field) ->
     position_nomsg(binary:first(Field), Field); 
+info_dispatch_type($=, Field) ->
+    position_msg(binary:first(Field), Field); 
+info_dispatch_type($/, Field) ->
+    position_time_nomsg(binary:first(Field), Field); 
+info_dispatch_type($@, Field) ->
+    position_time_msg(binary:first(Field), Field); 
 info_dispatch_type($T, Field) -> {telemetry, Field};
 info_dispatch_type($:, Field) -> {message, Field};
 info_dispatch_type($?, Field) -> {query, Field};
@@ -33,12 +39,36 @@ info_dispatch_type($), Field) -> {item, Field};
 info_dispatch_type($$, Field) -> {nmea, Field};
 info_dispatch_type($,, Field) -> {test_data, Field};
 info_dispatch_type($}, Field) -> {third_party, Field};
-info_dispatch_type(_, Field) -> Field, undefined.
+info_dispatch_type(_, Field) -> {undefined, Field}.
 
 position_nomsg(T, Field) when $0 =< T, T =< $9 ->
     % uncompressed position format
-    {position_uncompressed, Field};
+    {position, no_message, {uncompressed, Field}};
 position_nomsg($/, Field) ->
     % compressed position format;
-    {position_compressed, Field};
+    {position, no_message, {compressed, Field}};
 position_nomsg(_, Field) -> {undefined, Field}.
+
+position_msg(T, Field) when $0 =< T, T =< $9 ->
+    % uncompressed position format
+    {position, message, {uncompressed, Field}};
+position_msg($/, Field) ->
+    % compressed position format;
+    {position, message, {compressed, Field}};
+position_msg(_, Field) -> {undefined, Field}.
+
+position_time_nomsg(T, Field) when $0 =< T, T =< $9 ->
+    % uncompressed position_time format
+    {position_time, no_message, {uncompressed, Field}};
+position_time_nomsg($/, Field) ->
+    % compressed position_time format;
+    {position_time, no_message, {compressed, Field}};
+position_time_nomsg(_, Field) -> {undefined, Field}.
+
+position_time_msg(T, Field) when $0 =< T, T =< $9 ->
+    % uncompressed position_time format
+    {position_time, message, {uncompressed, Field}};
+position_time_msg($/, Field) ->
+    % compressed position_time format;
+    {position_time, message, {compressed, Field}};
+position_time_msg(_, Field) -> {undefined, Field}.
