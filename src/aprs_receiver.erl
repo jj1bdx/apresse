64a0500a87c5bbb188a1ce89933f00c7680f6e75
Kenji Rikitake <kenji.rikitake@acm.org>
2018-05-27 12:39:01 +0900

    aprs_receiver.erl: use a process to report lat/long data in ets

--- src/aprs_receiver.erl
+++ src/aprs_receiver.erl
@@ -1,13 +1,20 @@
 -module(aprs_receiver).
-
 -export([start/0,
+         init/0,
          connect_dump/0]).
 
+-include_lib("stdlib/include/ms_transform.hrl").
+
 start() ->
+    spawn_link(?MODULE, init, []).
+
+init() ->
+    ets:new(aprs_positions, [set, protected, named_table]),
     loop().
 
 loop() ->
     connect_dump(),
+    timer:sleep(timer:seconds(rand:uniform(6) + 4)),
     loop().
 
 connect_dump() ->
@@ -15,30 +22,77 @@ connect_dump() ->
         [binary, {active, false}, {packet, line},
             {nodelay, true}, {keepalive, true}
         ]),
-    {ok, Prompt} = gen_tcp:recv(Socket, 0, 5000),
-    io:format("~s", [Prompt]),
+    {ok, _Prompt} = gen_tcp:recv(Socket, 0, 5000),
     ok = gen_tcp:send(Socket, "user N6BDX pass -1 vers apresse 0.01\n"),
-    C = connect_dump_receive_loop(Socket, 0, aprs_is_decode:init_cp(), true),
-    io:format("Total: ~p packets~n~n", [C]),
+    _C = connect_dump_receive_loop(Socket, 0, aprs_is_decode:init_cp(), true),
     ok = gen_tcp:close(Socket).
 
 connect_dump_receive_loop(_, C, _, false) -> C;
 connect_dump_receive_loop(S, C, CP, true) ->
-    case gen_tcp:recv(S, 0, 10000) of
-        {ok, D} ->
-            io:format("~s", [D]),
-            case binary:first(D) of
-                $# -> io:format("Comment: not a packet~n~n", []);
-                _ -> {Source, Destination, Relay, Info} = 
-                        aprs_is_decode:decode_header(D, CP),
-                        io:format("Source: ~s~nDestination: ~s~nRelay: ~p~nInfo: ~s~n",
-                            [Source, Destination, Relay, Info]),
-                        io:format("Decoded: ~p~n~n", 
-                            [aprs_is_decode:info_dispatch(Info)])
-            end;
-        {error, E} ->
-            io:format("Error: ~p~n~n", [E]),
-            connect_dump_receive_loop(S, C, CP, false)
-    end,
-    connect_dump_receive_loop(S, C + 1, CP, true).
+    State = case gen_tcp:recv(S, 0, 10000) of
+            {ok, D} ->
+                case binary:first(D) of
+                    $# -> comment; % do nothing
+                    _ -> put_ets(aprs_is_decode:decode_header(D, CP))
+                end;
+            {error, _E} ->
+                error
+            end,
+    Continue = case State of
+                comment -> true;
+                error -> false;
+                _ -> true
+            end,
+    cleanup_ets(C), 
+    connect_dump_receive_loop(S, C + 1, CP, Continue).
 
+put_ets({Source, _Dest, _Relay, Info}) ->
+    Time = erlang:monotonic_time(millisecond),
+    put_ets(Time, Source, parse_message(aprs_is_decode:info_dispatch(Info))).
+    
+parse_message( {undefined, _}) -> undefined;
+parse_message(
+    {position, no_message,
+        {uncompressed, {time, _, _, {{longlat, Lat, Long}, _, _}}}}) ->
+    {Lat, Long};
+parse_message(
+    {position, no_message,
+        {uncompressed, {{longlat, Lat, Long}, _, _}}}) ->
+    {Lat, Long};
+parse_message(
+    {position, no_message,
+        {compressed, {time, _, _, {{longlat, Lat, Long}, _, _}}}}) ->
+    {Lat, Long};
+parse_message(
+    {position, no_message,
+        {compressed, {{longlat, Lat, Long}, _, _}}}) ->
+    {Lat, Long};
+parse_message(
+    {position, message,
+        {uncompressed, {time, _, _, {{longlat, Lat, Long}, _, _}}}}) ->
+    {Lat, Long};
+parse_message(
+    {position, message,
+        {uncompressed, {{longlat, Lat, Long}, _, _}}}) ->
+    {Lat, Long};
+parse_message(
+    {position, message,
+        {compressed, {time, _, _, {{longlat, Lat, Long}, _, _}}}}) ->
+    {Lat, Long};
+parse_message(
+    {position, message,
+        {compressed, {{longlat, Lat, Long}, _, _}}}) ->
+    {Lat, Long};
+parse_message(_) -> undefined.
+
+put_ets(Time, Source, {Lat, Long}) ->
+    % io:format("~p~n", [{Time, Source, Lat, Long}]),
+    ets:insert(aprs_positions, {Time, Source, Lat, Long});
+put_ets(_Time, _Source, _) -> true. % do nothing
+
+cleanup_ets(C) when (C rem 1000) == 0 ->
+    T = erlang:monotonic_time(millisecond) - 180000,
+    ets:select_delete(
+        aprs_positions,
+        ets:fun2ms(fun({Time, _, _}) -> Time < T end));
+cleanup_ets(_) -> 0.
